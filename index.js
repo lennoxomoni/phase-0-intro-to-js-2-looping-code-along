96752874f7164dd3fd9a72c5eddffb15f524c7fa
lennox <lennoxngubo549@gmail.com>
2024-10-07 10:56:04 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+  
+    for (const name of names) {
+      const message = (`Thank you, ${name}, for the wonderful surprise gift!`);
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number); 
+      --number
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
